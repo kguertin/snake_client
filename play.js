81de9f0177804912d66fbff2c22445e2aa25e92d
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-12 19:41:09 +0000

    added ability to quit game using stdin

--- play.js
+++ play.js
@@ -1,4 +1,21 @@
 const {connect} = require('./client')
 
+const setUpInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+
+
+  return stdin;
+}
+
 console.log('connecting...');
 connect();
+setUpInput();

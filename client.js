39caf7f55423eca7464ba3b47ddfd1814ad0c461
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-12 19:22:30 +0000

    added movement to client side

--- client.js
+++ client.js
@@ -6,11 +6,20 @@ const connect = function() {
     host: '172.46.0.236',
     port: 50541
   });
-  
+  //Connection messages and name 
   conn.on('connect', () => {
     console.log('Successfully connected to game server.')
     conn.write('Name: KG');
   })
+// Movement 
+// ~Hardcoded~
+/*   conn.on('connect', () => {
+    conn.write('Move: up');
+    setInterval(() => {
+      conn.write('Move: up')
+    }, 50)
+  }) */
+
   
   conn.on('data', (data) => {
     console.log(data)

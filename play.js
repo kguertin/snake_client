080ccf1ab7e43d6c46ee108ea594b1a5a26dbbd3
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-12 19:47:55 +0000

    added input file and refactored

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
-const {connect} = require('./client')
-
-const setUpInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  const handleUserInput = stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-
-
-  return stdin;
-}
+const {connect} = require('./client');
+const {setUpInput} = require('./input');
 
 console.log('connecting...');
 connect();
